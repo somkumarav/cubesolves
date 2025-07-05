2dfd4a50893989798fbf7c0f74aa27e74d448cd5
somkumarav <somkumarav@gmail.com>
2025-07-05 21:01:45 +0530

    refactor: removed unwanted imports

--- src/provider-wrapper.tsx
+++ src/provider-wrapper.tsx
@@ -1,13 +1,10 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
-import { auth } from "@/auth";
 
 export default async function ProviderWrapper({
   children,
 }: {
   children: ReactNode;
 }) {
-  const session = await auth();
-
   return <SessionProvider>{children}</SessionProvider>;
 }
