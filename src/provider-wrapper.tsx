da5f3c5396a3a57434fc2d7641998159f67a9416
somkumarav <somkumarav@gmail.com>
2025-07-12 21:08:18 +0530

    impr: add session to session provider

--- src/provider-wrapper.tsx
+++ src/provider-wrapper.tsx
@@ -1,10 +1,12 @@
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
+import { auth } from "./auth";
 
 export default async function ProviderWrapper({
   children,
 }: {
   children: ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  const session = await auth();
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
