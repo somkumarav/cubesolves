914898ab89a025d609d76df93f269e5623b66fce
somkumarav <somkumarav@gmail.com>
2025-07-12 15:15:44 +0530

    impr: middleware to add signup route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,29 @@
-import { auth } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const isAuthenticated = !!req.auth;
-  const pathname = req.nextUrl.pathname;
-  const baseURL = req.nextUrl.origin;
+  try {
+    const isAuthenticated = !!req.auth;
+    const pathname = req.nextUrl.pathname;
+    const baseURL = req.nextUrl.origin;
+    const authRoutes = ["/auth/signin", "/auth/signup"];
 
-  if (!isAuthenticated && pathname === "/profile") {
-    const newUrl = new URL("/auth/signin", baseURL);
-    return Response.redirect(newUrl);
-  }
+    if (!isAuthenticated && pathname === "/profile") {
+      const newUrl = new URL("/auth/signin", baseURL);
+      return Response.redirect(newUrl);
+    }
 
-  if (isAuthenticated && pathname === "/auth/signin") {
-    const newUrl = new URL(baseURL);
-    return Response.redirect(newUrl);
+    if (isAuthenticated && authRoutes.includes(pathname)) {
+      const newUrl = new URL(baseURL);
+      return Response.redirect(newUrl);
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
 export const config = {
-  matcher: ["/auth/signin", "/profile"],
+  matcher: ["/auth/signin", "/profile", "/auth/signup"],
 };
