e6de556b6f2982ae0e6ec7df40dd8ed3ed630de2
somkumarav <somkumarav@gmail.com>
2025-12-22 22:39:19 +0000

    add: new getSolveSession action

--- src/actions/solve-session.ts
+++ src/actions/solve-session.ts
@@ -1,21 +1,24 @@
 "use server";
 import { SolveSession } from "@prisma/client";
-import { ServerActionReturnType } from "@/lib/api.types";
-import { withSession } from "@/lib/session";
-import { SuccessResponse } from "@/lib/success";
+import { ServerActionReturnType } from "@/lib/HOC/api.types";
+import { withSession } from "@/lib/HOC/session";
+import { SuccessResponse } from "@/lib/HOC/success";
 import { zodSafeParser } from "@/lib/zod-validator";
 import {
-  createSolveSession,
-  createSolveSessionSchema,
+  AddSolveSession,
+  addSolveSessionSchema,
+  GetSolveSession,
+  getSolveSessionSchema,
 } from "@/schemas/solve-session";
 import { db } from "@/lib/prisma";
 import { generateDefaultSessionName } from "@/lib/solve-session";
+import { ErrorHandler } from "../lib/HOC/errors";
 
 export const addSolveSession = withSession<
-  createSolveSession,
+  AddSolveSession,
   ServerActionReturnType<SolveSession>
 >(async (session, data) => {
-  const validatedData = zodSafeParser(data, createSolveSessionSchema);
+  const validatedData = zodSafeParser(data, addSolveSessionSchema);
 
   const sessionName =
     validatedData.name || generateDefaultSessionName(validatedData.cube);
@@ -34,4 +37,31 @@ export const addSolveSession = withSession<
   });
 
   return new SuccessResponse("Solve added", 200, newSolveSession).serialize();
+});
+
+export const getSolveSession = withSession<
+  GetSolveSession,
+  ServerActionReturnType<SolveSession>
+>(async (session, data) => {
+  const validatedData = zodSafeParser(data, getSolveSessionSchema);
+
+  const solveSession = await db.solveSession.findFirst({
+    where: {
+      id: validatedData.solveSessionId,
+      userId: session.user?.id,
+    },
+    include: {
+      solves: true,
+    },
+  });
+
+  if (!solveSession) {
+    throw new ErrorHandler("Solve session not found", "NOT_FOUND");
+  }
+
+  return new SuccessResponse(
+    "Solve session found",
+    200,
+    solveSession
+  ).serialize();
 });
--- src/schemas/solve-session.ts
+++ src/schemas/solve-session.ts
@@ -1,9 +1,14 @@
 import z from "zod";
 import { CubeType } from "@prisma/client";
 
-export const createSolveSessionSchema = z.object({
+export const addSolveSessionSchema = z.object({
   name: z.string().min(1).max(100).optional(),
   cube: z.nativeEnum(CubeType),
 });
+export type AddSolveSession = z.infer<typeof addSolveSessionSchema>;
 
-export type createSolveSession = z.infer<typeof createSolveSessionSchema>;
+export const getSolveSessionSchema = z.object({
+  solveSessionId: z.string(),
+});
+
+export type GetSolveSession = z.infer<typeof getSolveSessionSchema>;
